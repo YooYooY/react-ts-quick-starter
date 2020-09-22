8b7ff3c3a35e47aef2840bf9ec1dceb098a85938
chenwl <13751840799@163.com>
2020-09-22 16:38:37 +0800

    feat: 调整

--- src/inedx.ts
+++ src/inedx.ts
@@ -1,7 +1,12 @@
-const a = (num1, num2) => {
-  console.log(num1);
+const config = {
+	name: 'chenwl',
+	age: 12
+};
 
-  return num1 + num2;
+const a = (num1, num2) => {
+	console.log(num1);
+
+	return num1 + num2 + config.age;
 };
 
 export default a;
